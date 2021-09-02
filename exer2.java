a6ab4bc6871366b7dc435692ed2678aec2dce225
eduborbolan <e.borbolan@gmail.com>
2021-09-02 18:59:20 +0000

    adicionando

---
+++ exer2.java
@@ -0,0 +1,15 @@
+public class exer2{
+    public static void main (String[]attrs){
+        int x=13;
+        While(x!=1); {
+        if(x%2==0){
+            x=x/2;
+        }
+        else {
+            x = 3*x+1;
+        }
+    }
+    }
+
+    System.out.println(x + "->");
+}
